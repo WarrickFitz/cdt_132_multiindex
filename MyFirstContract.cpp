788dbdce8d36560dd876d1f843b8fb99dc7e9a25
Warrick FitzGerald <warrick@eosnewyork.io>
2018-11-04 20:31:59 -0500

    addresses.find error

--- MyFirstContract.cpp
+++ MyFirstContract.cpp
@@ -1,8 +1,32 @@
 #include <eosiolib/eosio.hpp>
+#include <eosiolib/print.hpp>
 #include "MyFirstContract.hpp"
 
+using namespace eosio;
 
-void MyFirstContract::hi(eosio::name user) {
-	int x = 123;
+//https://developers.eos.io/eosio-home/docs/data-persistence#section-step-6-the-constructor
+void MyFirstContract::upsert(eosio::name user, uint16_t win_count, uint16_t lost_count) {
+	require_auth( user );
+	users_table addresses(_code, _code.value);
+	
+	//auto iterator = addresses.find(user.value);  << This causes a problem
+
+/*	
+	if( iterator == addresses.end() )
+	{
+		addresses.emplace(user, [&]( auto& row ) {
+		row.name = user;
+		row.win_count = win_count;
+		row.lost_count = lost_count;
+		});
+	}
+	else {
+		std::string changes;
+		addresses.modify(iterator, user, [&]( auto& row ) {
+		row.name = user;
+		row.win_count = win_count;
+		row.lost_count = lost_count;
+		});
+	}
+*/
 }
-
--- MyFirstContract.hpp
+++ MyFirstContract.hpp
@@ -4,24 +4,23 @@ using namespace eosio;
 
 CONTRACT MyFirstContract : public eosio::contract {
 
+  public:
+    using contract::contract;
+    MyFirstContract(name receiver, name code,  datastream<const char*> ds):contract(receiver, code, ds) {}
+    ACTION upsert(name user, uint16_t win_count, uint16_t lost_count); 
+
   private:
     
     struct [[eosio::table]] user_info {
-      name            name;
+      name            key;
       uint16_t        win_count = 0;
       uint16_t        lost_count = 0;
 
-      auto primary_key() const { return name; }
+      auto primary_key() const { return key; }
     };
 
     typedef eosio::multi_index<name("users"), user_info> users_table;
 
-    users_table _users;
-
-  public:
-    using contract::contract;
-    ACTION hi(name user);
-
 };
 
-EOSIO_DISPATCH(MyFirstContract, (hi))
+EOSIO_DISPATCH(MyFirstContract, (upsert))
