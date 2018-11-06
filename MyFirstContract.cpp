fe56009c5815dbc224e7ffe1bd783299f969483e
Warrick FitzGerald <warrick@eosnewyork.io>
2018-11-06 09:59:45 -0500

    Fix and Added echo

--- MyFirstContract.cpp
+++ MyFirstContract.cpp
@@ -4,29 +4,32 @@
 
 using namespace eosio;
 
-//https://developers.eos.io/eosio-home/docs/data-persistence#section-step-6-the-constructor
-void MyFirstContract::upsert(eosio::name user, uint16_t win_count, uint16_t lost_count) {
-	require_auth( user );
-	users_table addresses(_code, _code.value);
-	
-	//auto iterator = addresses.find(user.value);  << This causes a problem
 
-/*	
-	if( iterator == addresses.end() )
+void MyFirstContract::upsert(eosio::name user, uint16_t win_count, uint16_t lost_count, std::string note) {
+	//For testing purposes I'll let anyone push data into this table 
+	//require_auth( _self ); 
+	users_table users(_self, user.value);
+	
+	auto iterator = users.find(user.value);  
+
+	if( iterator == users.end() )
 	{
-		addresses.emplace(user, [&]( auto& row ) {
-		row.name = user;
+		users.emplace(user, [&]( auto& row ) {
+		row.user = user.value;
 		row.win_count = win_count;
 		row.lost_count = lost_count;
 		});
 	}
 	else {
-		std::string changes;
-		addresses.modify(iterator, user, [&]( auto& row ) {
-		row.name = user;
+		users.modify(iterator, user, [&]( auto& row ) {
+		row.user = user.value;
 		row.win_count = win_count;
 		row.lost_count = lost_count;
 		});
 	}
-*/
+
+}
+
+void MyFirstContract::echo(std::string message) {
+	print("Echo: ", message);
 }
--- MyFirstContract.hpp
+++ MyFirstContract.hpp
@@ -6,21 +6,23 @@ CONTRACT MyFirstContract : public eosio::contract {
 
   public:
     using contract::contract;
+    //https://developers.eos.io/eosio-home/docs/data-persistence#section-step-6-the-constructor
     MyFirstContract(name receiver, name code,  datastream<const char*> ds):contract(receiver, code, ds) {}
-    ACTION upsert(name user, uint16_t win_count, uint16_t lost_count); 
+    ACTION upsert(name user, uint16_t win_count, uint16_t lost_count, std::string note); 
+    ACTION echo(std::string message); 
 
   private:
     
-    struct [[eosio::table]] user_info {
-      name            key;
+    TABLE user_info {
+      uint64_t        user;
       uint16_t        win_count = 0;
       uint16_t        lost_count = 0;
 
-      auto primary_key() const { return key; }
+      auto primary_key() const { return user; }
     };
 
     typedef eosio::multi_index<name("users"), user_info> users_table;
 
 };
 
-EOSIO_DISPATCH(MyFirstContract, (upsert))
+EOSIO_DISPATCH(MyFirstContract, (upsert)(echo))
